6d89c8d0024683cd5039153b9ca73f0c5cf0b18d
Fallingstash <sashkaprog228@gmail.com>
2024-09-25 19:26:03 +0700

    Add files via upload

---
+++ LABALABA.cpp
@@ -0,0 +1,17 @@
+
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+    double TIME[9] = {0, 15, 30, 45, 60, 120, 180, 240, 300};
+    double t0 = 100 ;
+    double tv = 20;
+    int i = 0;
+    while (i < 8) {
+    int T = TIME[i];
+    double t = tv + (t0 - tv) * exp(-T/20);
+    cout << "СПУСТЯ ВРЕМЯ  " << T << " МИНУТ ТЕМПЕРАТУРА ПЛИТЫ " << t << " ГРАДУСОВ ЦЕЛЬСИЯ" << endl;
+    i++;
+}}
